0bca0e5a0f360582ca54368df911e3921db52007
Markus Heldrup <markusheldrup@hotmail.com>
2020-03-16 10:49:10 +0100

    added the second clipping and animation of incoming element

--- src/js/components/ClipNslide/MainContainer.jsx
+++ src/js/components/ClipNslide/MainContainer.jsx
@@ -5,8 +5,9 @@ const MainContainer = props => {
   const [clipNslide, setClipNslide] = useState({
     displayChild: 0,
     clippedElements: [],
+    clippedIncomingElements: [],
     animation: false,
-    finishedClippig: false,
+    finishedClippig: false
   });
 
   const handleChangeSlide = event => {
@@ -17,7 +18,7 @@ const MainContainer = props => {
 
     let newDisplayChild = (clipNslide.displayChild + 1) % props.children.length;
 
-    // 0.3 is 30% of the screen to go to previous child
+    // 0.3 is 30% of the screen to go to previous child in the slider
     if (event.clientX < windowWidth * 0.3) {
       newDisplayChild =
         clipNslide.displayChild - 1 < 0
@@ -25,12 +26,13 @@ const MainContainer = props => {
           : clipNslide.displayChild - 1;
     }
 
-    const clippedElements = clipAnimationElements();
+    const {clippedElements, clippedIncomingElements} = clipAnimationElements();
 
     setClipNslide({
       ...clipNslide,
       displayChild: newDisplayChild,
       clippedElements,
+      clippedIncomingElements,
       animation: true,
       finishedClippig: true
     });
@@ -40,38 +42,52 @@ const MainContainer = props => {
     // const windowHeight =
     //   window.innerHeight > 0 ? window.innerHeight : window.screen.height;
 
-    const currentChild = props.children[clipNslide.displayChild];
-
     const divideBy = props.divideBy ? props.divideBy : 3;
-    const clippedElements = [];
+    const clippedElements = [],
+      clippedIncomingElements = [];
 
     for (let i = 1; i <= divideBy; i++) {
+      const stylesObj = {
+        clipPath: `polygon(0 ${((i - 1) * 100) / divideBy}%, 0% ${(i * 100) /
+          divideBy}%, 100% ${(i * 100) / divideBy}%, 100% ${((i - 1) * 100) /
+          divideBy}%)`,
+        zIndex: `${5 + i}`,
+        transition: `left ${i % 2 == 0 ? Math.random() * 0.4 + 1.3 : Math.random() * 0.3 + 1}s ease-in`,
+        transitionDelay: `${i === 1 ? 0 : Math.random() * 0.5}s`
+      };
+
       clippedElements.push(
         <div
           key={i}
           style={{
-            clipPath: `polygon(0 ${((i - 1) * 100) / divideBy}%, 0% ${(i *
-              100) /
-              divideBy}%, 100% ${(i * 100) / divideBy}%, 100% ${((i - 1) *
-              100) /
-              divideBy}%)`,
-            zIndex: `${5 + i}`,
-            transition: `left ${i%2==0 ? Math.random()*0.5+1.3 : Math.random()*0.3+1}s ease-in`,
-            transitionDelay: `${i === 1 ? 0 : Math.random()*0.7}s`,
+            ...stylesObj,
             left: "0%"
           }}
           className="clipped-element"
         >
-          {currentChild}
+          {props.children[clipNslide.displayChild]}
+        </div>
+      );
+
+      clippedIncomingElements.push(
+        <div
+          key={i}
+          style={{
+            ...stylesObj,
+            left: "160%"
+          }}
+          className="clipped-element"
+        >
+          {props.children[clipNslide.displayChild+1]}
         </div>
       );
     }
 
-    return clippedElements;
+    return {clippedElements, clippedIncomingElements};
   };
 
   const runAnimation = () => {
-    const { clippedElements } = clipNslide;
+    const { clippedElements, clippedIncomingElements } = clipNslide;
 
     const newClippedElements = clippedElements.map(element => {
       return (
@@ -80,16 +96,30 @@ const MainContainer = props => {
           key={element.key}
           style={{
             ...element.props.style,
-            left: '-100%',
+            left: "-160%"
           }}
         />
-      )
+      );
+    });
+
+    const newClippedIncomingElements = clippedIncomingElements.map(element => {
+      return (
+        <element.type
+          {...element.props}
+          key={element.key}
+          style={{
+            ...element.props.style,
+            left: "0%"
+          }}
+        />
+      );
     });
 
     setClipNslide({
       ...clipNslide,
       finishedClippig: false,
-      clippedElements:newClippedElements
+      clippedElements: newClippedElements,
+      clippedIncomingElements: newClippedIncomingElements
     });
   };
 
@@ -99,6 +129,7 @@ const MainContainer = props => {
     }
   });
 
+  //Sets the background to either a color or a url depending on what is given by the user
   const backgroundStyle = /(url\()|(\/)/g.test(props.background)
     ? { backgroundImage: props.background }
     : { backgroundColor: props.background };
@@ -112,6 +143,7 @@ const MainContainer = props => {
       {clipNslide.animation
         ? clipNslide.clippedElements
         : props.children[clipNslide.displayChild]}
+      {clipNslide.animation ? clipNslide.clippedIncomingElements : ""}
     </div>
   );
 };
